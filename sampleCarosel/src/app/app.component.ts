1ab5c3eb26c99fc578db37d8ee96d9ffb9f93ac5
tauhidbs23 <tauhidul.islam@brainstation23.com>
2020-10-21 17:17:41 +0600

    added angular

--- sampleCarosel/src/app/app.component.ts
+++ sampleCarosel/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sampleCarosel';
+  bookData2 ={
+    book:{
+      bookDealImages:{
+        imglink:'oiisjoidfjsdoif'
+      }
+    }
+  }
+
+
 }
